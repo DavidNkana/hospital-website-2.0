08d0f3eb4d0dfc5c145a5088c12eb603abae0a0f
David Nkana <davidnkana74@gmail.com>
2021-09-10 01:28:37 -0700

    initial commit

--- hospital website/js/script.js
+++ hospital website/js/script.js
@@ -1,12 +1,10 @@
-let menu = document.querySelector('#menu-btn');
-let navbar = document.querySelector('.navbar');
-
-menu.onclick = () =>{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-}
-
-window.onscroll = () =>{
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
-}
+$(document).ready(function(){
+    $('#menu-btn').click(function(){
+        $('.navbar').toggleClass('active')
+        $('#menu-btn').toggleClass('fa-times')
+    })
+    $(window).scroll(function(){
+        $('.navbar').removeClass('active')
+        $('#menu-btn').removeClass('fa-times')
+    })
+})
